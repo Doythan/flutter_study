9b33fd1e327668b5ce7aa1ce2a192da580772f86
DGW <won03289@gmail.com>
2025-03-12 03:01:18 +0900

    feat: MVVM 패턴과 Riverpod 상태관리를 연습하기 위한 Todo 앱

--- practice_app/lib/main.dart
+++ practice_app/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final counterProvider = StateProvider<int>((ref) => 0); // 전역 상태(Provider)
+import 'feature/todo/presentation/screens/todo_screen.dart';
 
 void main() {
   runApp(ProviderScope(child: MyApp()));
@@ -13,38 +12,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: CounterScreen(),
-    );
-  }
-}
-
-class CounterScreen extends ConsumerWidget {
-  const CounterScreen({super.key});
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final count = ref.watch(counterProvider);
-
-    return Scaffold(
-      appBar: AppBar(title: Text("Riverpod 예제")),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "카운트 $count",
-              style: TextStyle(fontSize: 24),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            ElevatedButton(
-                onPressed: () =>
-                    ref.read(counterProvider.notifier).state++, // 상태변경
-                child: Text("증가"))
-          ],
-        ),
-      ),
+      title: "Flutter MVVM with Riverpod Todo App",
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: TodoScreen(),
     );
   }
 }
